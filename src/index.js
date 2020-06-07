993b0eea614fe374c2283db738ee38105b1dd9cb
jayson_kesterson <your_email@example.com>
2020-06-07 10:59:03 -0400

    displaying quiz name by category

--- src/index.js
+++ src/index.js
@@ -2,13 +2,41 @@ const endPoint = "http://localhost:3000/api/v1/quizzes"
 const questionEndPoint = "http://localhost:3000/api/v1/questions"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getQuizzes()
+    quizGenieHome()
 
     const createQuizForm = document.querySelector("#quiz-form-container")
 
     createQuizForm.addEventListener("submit", (e) => createFormHandler(e))
 
 })
+
+function quizGenieHome() {
+
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(quizzes => {
+        quizzes.data.forEach(quiz => {
+            let newQuiz = new Quiz(quiz.id, quiz.attributes)
+        })
+    })
+
+    const sportsQuizzesData = Quiz.findByCategory("sports")
+    const randomKnowQuizzes = Quiz.findByCategory("Random Knowledge")
+    const musicQuizzes = Quiz.findByCategory("music")
+
+
+    const quizContainer = document.getElementById("quiz-container");
+    const title = document.createElement("h1")
+    title.innerText = "Welcome to QuizGenie!"
+    
+    const sportsQuizzes = document.createElement("h3")
+    sportsQuizzesData.forEach(quiz => {
+        sportsQuizzes.textContent += quiz.name
+    })
+
+    quizContainer.appendChild(title)
+    quizContainer.appendChild(sportsQuizzes)
+}
 
 function createFormHandler(e) {
     e.preventDefault()
@@ -51,10 +79,12 @@ function postFetch(name, category) {
 }
     
 
-
-
 function playQuiz() {
+    const quizzesToPlay = document.querySelectorAll(".play")
+    quizzesToPlay.addEventListener("click", (e) => liveQuiz(e))
+}
 
+function liveQuiz() {
 }
 
 function getQuizzes() {
